18e27e07280afda7098d72e15d5a4678fd4f88e3
xalocc <146940476+xalocc@users.noreply.github.com>
2023-10-27 20:32:46 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,20 +1,22 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝚃𝙷𝙴 𝚂𝙷𝙰𝙳𝙾𝚆 𝙱𝚁𝙾𝙺𝙴𝚁𝚂 - 𝚃𝙴𝙰𝙼 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
+  const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 LA COMUNIDAD DKN*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj
+*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 de vs:*
+*1.-* https://chat.whatsapp.com/EZofeD04M7y4xeWVsXbYz2
+*2.-* https://chat.whatsapp.com/CjyxXfxKcoG6E98NDmLNI1
+*3.-* https://chat.whatsapp.com/BZrhgXP9nRhEtjtZ7FYDIq
+*4.-* https://chat.whatsapp.com/EO3DJHJAwJ3K7ledcZCJsU
+*5.-* https://chat.whatsapp.com/Km0lgn5N5WC1loqScNiMFv
+*6.-* https://chat.whatsapp.com/BdnmCIWfBEgAQO5LdpAM4i
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
-
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
-
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-*5.-* https://chat.whatsapp.com/DLrFJUPxEQ2IrOm7sJWJ0k`.trim();
+*➤ Eventos paga:*
+*1.-* https://chat.whatsapp.com/GlR796cUzht2oy81N3jK6h
+*2.-* https://chat.whatsapp.com/JYYd1Y8d8k7EkvESlv4OGn
+*3.-* https://chat.whatsapp.com/Dxk6g2YawEgJrnfg2GFMDy`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://www.instagram.com/comunidad.dkn`},
     'mimetype': `application/${document}`,
     'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
     'fileLength': 99999999999999,
@@ -23,13 +25,13 @@ const handler = async (m, {conn, usedPrefix}) => {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://www.instagram.com/comunidad.dkn',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': 'https://www.instagram.com/comunidad.dkn'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
